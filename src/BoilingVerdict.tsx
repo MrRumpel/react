b23891114d7bdf68394763aef9a335fb1bb8be73
Rumpel <hades137747575@gmail.com>
2019-08-08 16:09:43 +0800

    状态提升起步

---
+++ src/BoilingVerdict.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface Props {
+    celsius: number,
+}
+
+export const BoilingVerdict: React.SFC<Props> = props => {
+    if (props.celsius >= 100) {
+        return <p>The water would boil.</p>;
+    }
+    return <p>The water would not boil.</p>;
+};
