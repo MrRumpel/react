f169284b9ecf6ee06c050a8beb3c8fc930afd097
Rumpel <hades137747575@gmail.com>
2019-08-13 14:19:01 +0800

    啊 RouteConfig

--- src/route/Routes.tsx
+++ src/route/Routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
-import { renderRoutes } from "react-router-config";
+import { renderRoutes, RouteConfig } from "react-router-config";
 
 interface Props { }
 
@@ -9,7 +9,7 @@ export const Routes: React.SFC<Props> = props => {
     <BrowserRouter>{renderRoutes(routes)}</BrowserRouter>
   );
 };
-const Root = ({ route }: any) => (
+const Root: React.SFC = ({ route }: RouteConfig) => (
   <div>
     <h1>Root</h1>
     {/* child routes won't render without this */}
@@ -17,13 +17,13 @@ const Root = ({ route }: any) => (
   </div>
 );
 
-const Home = ({ route }: any) => (
+const Home: React.SFC = ({ route }: RouteConfig) => (
   <div>
     <h2>Home</h2>
   </div>
 );
 
-const Child = ({ route }: any) => (
+const Child: React.SFC = ({ route }: RouteConfig) => (
   <div>
     <h2>Child</h2>
     {/* child routes won't render without this */}
@@ -31,14 +31,14 @@ const Child = ({ route }: any) => (
   </div>
 );
 
-const GrandChild = ({ someProp }: any) => (
+const GrandChild = ({ someProp }: RouteConfig) => (
   <div>
     <h3>Grand Child</h3>
     <div>{someProp}</div>
   </div>
 );
 
-const routes = [
+const routes: RouteConfig[] = [
   {
     component: Root,
     routes: [
